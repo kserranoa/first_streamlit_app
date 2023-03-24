3aef593a61cbb857cd27b9f4b7dcecbb22bf24dd
Karina Serrano <52381907+kserranoa@users.noreply.github.com>
2023-03-24 09:25:39 -0600

    Update streamlit_app.py
    change banana default to jackfruit

--- streamlit_app.py
+++ streamlit_app.py
@@ -47,5 +47,5 @@ streamlit.header("The Fruit Load List contains: ")
 streamlit.dataframe(my_data_row)
 
 # Allow the user to add a fruit to the list
-add_fruit = streamlit.text_input('What fruit would you like to add?','Banana')
+add_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
 streamlit.write('The user would like to add', add_fruit)
