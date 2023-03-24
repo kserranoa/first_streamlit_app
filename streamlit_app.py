05e374d84ed029d64f286ab97f6d24d8b34c6744
Karina Serrano <52381907+kserranoa@users.noreply.github.com>
2023-03-24 12:50:48 -0600

    Update streamlit_app.py
    rearrange code

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,16 +16,13 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
-
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 streamlit.dataframe(my_fruit_list)
 
 #Let's put a pick list here so they can pick the fruit they want to include
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
-#display the table on the page
-streamlit.dataframe(fruits_to_show)
+streamlit.dataframe(fruits_to_show)#display the table on the page
 
 #create the repeatable code block(called a function)
 def get_fruityvice_data(this_fruit_choice):
@@ -36,7 +33,7 @@ def get_fruityvice_data(this_fruit_choice):
 #New section to display fruit vice API response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
-  fruit_choice = streamlit.text_input('What fruit would you like information LINDA?')
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error("Please select a fruit to get information")
   else:
